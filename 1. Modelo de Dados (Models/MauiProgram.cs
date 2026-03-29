ef6143215fd3625fefd0492e36e3339feb26439b
marceloamorimjrr-sys <marcelo.amorim.jrr@gmail.com>
2026-03-28 22:28:10 -0300

    Create MauiProgram.cs
    Este é o primeiro arquivo que o Windows lê. Ele é o "porteiro" que configura as fontes e as bibliotecas do SQLite.

---
+++ 1. Modelo de Dados (Models/MauiProgram.cs
@@ -0,0 +1,13 @@
+public static MauiApp CreateMauiApp()
+{
+    var builder = MauiApp.CreateBuilder();
+    builder
+        .UseMauiApp<App>()
+        .ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        });
+
+    return builder.Build();
+}
