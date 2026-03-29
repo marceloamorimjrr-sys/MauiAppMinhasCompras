fc046cb6cb2993853b31cd9b22cf86a5804b621a
marceloamorimjrr-sys <marcelo.amorim.jrr@gmail.com>
2026-03-28 22:24:28 -0300

    Create SQLiteDatabaseHelper.cs
    2. Motor do Banco (Helpers/SQLiteDatabaseHelper.cs)

---
+++ 1. Modelo de Dados (Models/Models/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,21 @@
+using SQLite;
+using MauiAppMinhasCompras.Models;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+    }
+}
